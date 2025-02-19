e26b4e91e85011cd0c416c76e80508789ee6a025
eshellster <eshellster@gmail.com>
2025-02-19 19:07:58 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,157 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [score, setScore] = useState(0);
+  const CanvasSize = 400;
+  const CellSize = 10;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const gameSpeed = 150;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const snakeRef = useRef([{ x: 200, y: 200 }]);
+  const directionRef = useRef({ dx: 0, dy: -10 });
+  const foodRef = useRef({ x: 0, y: 0 });
+
+  const gameIntervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        // 먹이생성
+        generateFood(ctx);
+
+        gameIntervalRef.current = setInterval(() => {
+          gameLoop(ctx); // 게임 시작
+        }, gameSpeed);
+
+        //키보드제어
+        const handleKeyDown = (e: KeyboardEvent) => changeDirection(e);
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+          clearInterval(gameIntervalRef.current as NodeJS.Timeout);
+          window.removeEventListener("keydown", handleKeyDown);
+        };
+      }
+    }
+  }, []);
+
+  const gameLoop = (ctx: CanvasRenderingContext2D) => {
+    setTimeout(() => {
+      clearCanvas(ctx);
+      drawSnake(ctx);
+      drawFood(ctx);
+      moveSnake(ctx);
+    }, 0);
+
+    if (checkCollision()) {
+      clearInterval(gameIntervalRef.current as NodeJS.Timeout);
+      alert("게임 오버");
+    }
+    // 다음 프레임 요청
+  };
+  const changeDirection = (e: KeyboardEvent) => {
+    const { dx, dy } = directionRef.current;
+    switch (e.key) {
+      case "ArrowUp":
+      case "w":
+        if (dy == 0) directionRef.current = { dx: 0, dy: -CellSize };
+        break;
+      case "ArrowDown":
+      case "s":
+        if (dy == 0) directionRef.current = { dx: 0, dy: CellSize };
+        break;
+      case "ArrowLeft":
+      case "a":
+        if (dx == 0) directionRef.current = { dx: -CellSize, dy: 0 };
+        break;
+      case "ArrowRight":
+      case "d":
+        if (dx == 0) directionRef.current = { dx: CellSize, dy: 0 };
+        break;
+      default:
+        break;
+    }
+  };
+  const checkCollision = () => {
+    const snake = snakeRef.current;
+    const head = snake[0];
+
+    if (
+      head.x < 0 ||
+      head.x >= CanvasSize ||
+      head.y < 0 ||
+      head.y >= CanvasSize
+    ) {
+      return true;
+    }
+
+    // 머리와 몸 충돌검사
+    for (let i = 1; i < snake.length; i++) {
+      if (head.x === snake[i].x && head.y === snake[i].y) return true;
+    }
+    return false;
+  };
+  const moveSnake = (ctx: CanvasRenderingContext2D) => {
+    const snake = snakeRef.current;
+    const head = { ...snake[0] };
+    const { dx, dy } = directionRef.current;
+    head.x += dx;
+    head.y += dy;
+    snake.unshift(head);
+    if (head.x === foodRef.current.x && head.y === foodRef.current.y) {
+      setScore((prev) => prev + 10);
+      generateFood(ctx);
+    } else {
+      snake.pop();
+    }
+  };
+  const drawSnake = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "lightblue";
+    ctx.strokeStyle = "darkblue";
+    snakeRef.current.forEach((segment) => {
+      ctx.fillRect(segment.x, segment.y, CellSize, CellSize);
+      ctx.strokeRect(segment.x, segment.y, CellSize, CellSize);
+    });
+  };
+  const generateFood = (ctx: CanvasRenderingContext2D) => {
+    const maxCells = CanvasSize / CellSize;
+    const foodX = Math.floor(Math.random() * maxCells) * CellSize;
+    const foodY = Math.floor(Math.random() * maxCells) * CellSize;
+    foodRef.current = { x: foodX, y: foodY };
+
+    ctx.fillStyle = "lightgreen";
+    ctx.strokeStyle = "darkgreen";
+    ctx.fillRect(foodRef.current.x, foodRef.current.y, CellSize, CellSize);
+    ctx.strokeRect(foodRef.current.x, foodRef.current.y, CellSize, CellSize);
+  };
+
+  const drawFood = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "lightgreen";
+    ctx.strokeStyle = "darkgreen";
+    ctx.fillRect(foodRef.current.x, foodRef.current.y, CellSize, CellSize);
+    ctx.strokeRect(foodRef.current.x, foodRef.current.y, CellSize, CellSize);
+  };
+
+  const clearCanvas = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CanvasSize, CanvasSize);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(0, 0, CanvasSize, CanvasSize);
+  };
+  return (
+    <div className="w-full h-screen flex flex-col gap-8 justify-center items-center ">
+      <h1 className="text-4xl font-extrabold">Nextjs 스네이크 게임</h1>
+      <h2 className='"text-4xl font-extrabold'>{score}</h2>
+      <canvas
+        ref={canvasRef}
+        className="bg-white"
+        width={CanvasSize}
+        height={CanvasSize}
+      />
     </div>
   );
 }
